46f705299b99c8bf616ffe3c172806a5c623d80a
Muhammed İzzet DEMİR <muhammeddemir@papilon.com.tr>
2024-10-19 12:05:44 +0300

    added subtask design for TaskCard

--- src/components/WorkspaceAndBoard/TaskCard.js
+++ src/components/WorkspaceAndBoard/TaskCard.js
@@ -8,18 +8,13 @@ import miniCalendar from '../../assets/mini-calendar.svg';
 import fork from '../../assets/fork-blue.svg';
 import people from '../../assets/people-blue.svg';
 import flag from '../../assets/flag.svg';
-console.log()
-function TaskCard ({ colIndex, taskIndex, color }) {
-  console.log("colIndex", colIndex);
-  console.log("taskIndex", taskIndex);
-  console.log("color", color);
+import downArrow from '../../assets/down-arrow.svg';
+
+function TaskCard({ colIndex, taskIndex, color }) {
   const boards = useSelector((state) => state.auth.boards);
-  console.log("boards", boards);
   const board = boards.find((board) => board.isActive === true);
   const columns = board.columns;
-  console.log("columns", columns);
   const col = columns.find((col, i) => i === colIndex);
-  console.log("col", col);
   const task = col.tasks.find((task, i) => i === taskIndex);
   const [isTaskModalOpen, setIsTaskModalOpen] = useState(false);
 
@@ -40,67 +35,72 @@ function TaskCard ({ colIndex, taskIndex, color }) {
 
   return (
     <div>
-    <div
-      onClick={() => {
-        setIsTaskModalOpen(true);
-      }}
-      draggable
-      onDragStart={handleOnDrag}
-      className="task-container"
-      style={{ borderColor: color }} // Use the passed color here
-    >
-      <div className="task-frame">
-        <div className="task-content">
-          <p className="task-title">{task.title}</p>
-          <div className="task-icon">
-            <img src={rightButton} alt="icon" />
+      <div
+        onClick={() => {
+          setIsTaskModalOpen(true);
+        }}
+        draggable
+        onDragStart={handleOnDrag}
+        className="task-container"
+        style={{ borderColor: color }} // Use the passed color here
+      >
+        <div className="task-frame">
+          <div className="task-content">
+            <p className="task-title">{task.title}</p>
+            <div className="task-icon">
+              <img src={rightButton} alt="icon" />
+            </div>
+          </div>
+          <div className="task-divider"></div>
+        </div>
+
+        <div className="task-info">
+          <div className="task-date">
+            <div className="mini-calendar-icon">
+              <img src={miniCalendar} alt="mini calendar" />
+            </div>
+            <span className="date-text">August 12</span>
+          </div>
+          <div className="task-status">
+            <span>%50</span>
           </div>
         </div>
-        <div className="task-divider"></div>
+
+        <div className="task-stats">
+          <div className="stat-group">
+            <div className="stat-icon">
+              <img src={fork} alt="Fork icon" />
+            </div>
+            <div className="stat-text">8</div>
+          </div>
+
+          <div className="stat-group">
+            <div className="stat-icon">
+              <img src={people} alt="People icon" />
+            </div>
+            <div className="stat-text">4</div>
+          </div>
+          <div className="task-extra">
+            <img src={flag} alt="Right button" />
+          </div>
+        </div>
       </div>
 
-      <div className="task-info">
-        <div className="task-date">
-          <div className="mini-calendar-icon">
-            <img src={miniCalendar} alt="mini calendar" />
-          </div>
-          <span className="date-text">August 12</span>
-        </div>
-        <div className="task-status">
-          <span>%50</span>
-        </div>
+      {/* Task container'ın altına eklenen footer */}
+      <div className="task-footer">
+        <span className="footer-text"></span>
+        <img src={downArrow} alt="Extra icon" className="footer-icon" />
       </div>
 
-      <div className="task-stats">
-        <div className="stat-group">
-          <div className="stat-icon">
-            <img src={fork} alt="Fork icon" />
-          </div>
-          <div className="stat-text">8</div>
-        </div>
-
-        <div className="stat-group">
-          <div className="stat-icon">
-            <img src={people} alt="People icon" />
-          </div>
-          <div className="stat-text">4</div>
-        </div>
-        <div className="task-extra">
-          <img src={flag} alt="Right button" />
-        </div>
-      </div>
-
-      
-    </div>
-    {isTaskModalOpen && (
+      {isTaskModalOpen && (
         <TaskModal
           colIndex={colIndex}
           taskIndex={taskIndex}
           setIsTaskModalOpen={setIsTaskModalOpen}
         />
       )}
-  </div>
-);
+    </div>
+  );
 }
 
 export default TaskCard;
