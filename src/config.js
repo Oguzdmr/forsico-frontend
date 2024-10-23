ab001648dcf2cd653a49cbbed2cac384f101c52d
Oğuzhan Demir <doguzhan476@gmail.com>
2024-10-23 22:10:08 +0300

    update

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 const baseUrl = "https://auth-api-beg6ftbghybrhabq.eastus-01.azurewebsites.net";
 const authProdUrl = "https://authapi.forsico.io";
-const boardApiBaseUrl =
-  "http://localhost:3005" //"https://forsico-board-api-dzadc8hbeuegg9hv.eastus-01.azurewebsites.net";
+const boardApiBaseUrl ="https://forsico-board-api-dzadc8hbeuegg9hv.eastus-01.azurewebsites.net";
 const supportApiUrl = "https://support.forsico.io";
 const supportApiKey = "bebabd708e56b530141a6fdae5aa35b4";
 
