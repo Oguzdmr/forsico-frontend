0824c74b2009457784b71d32918691d20995b336
Oğuzhan Demir <doguzhan476@gmail.com>
2024-10-28 16:19:05 +0300

    updates

--- src/pages/WorkspacePages/Board.js
+++ src/pages/WorkspacePages/Board.js
@@ -1,6 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import TaskList from "../../components/WorkspaceAndBoard/TaskList";
 import VectorIcon from "../../assets/Vector.png";
+import MembersIcon from "../../assets/memberIcon.svg";
+import ShareIcon from "../../assets/shareIcon.svg";
+import FilterBoardIcon from "../../assets/filterBoard.svg";
+import AddMemberIcon from "../../assets/addMemberIcon.svg";
 import { useDispatch, useSelector } from "react-redux";
 import "../../styles/workspaceCss/board.css";
 import { fetchBoard, updateStatus } from "../../store/boardSlice";
@@ -25,7 +29,13 @@ function Board() {
 
   const { workspaceId } = useParams();
   const { boardId } = useParams();
+  const memberModalRef = useRef(null);
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
   useEffect(() => {
     dispatch(updateStatus({ status: "idle" }));
   }, [boardId]);
@@ -36,69 +46,124 @@ function Board() {
     }
   }, [dispatch, status]);
 
+
+
   useEffect(() => {
-    const handleWindowResize = () => {
-      setWindowSize([window.innerWidth, window.innerHeight]);
-    };
-
-    window.addEventListener("resize", handleWindowResize);
-
+    document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      window.removeEventListener("resize", handleWindowResize);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
-  });
+  }, []);
+
+  const handleClickOutside = (event) => {
+    if (
+      memberModalRef.current &&
+      !memberModalRef.current.contains(event.target)
+    ) {
+      setIsModalOpen(false);
+    }
+  };
 
   const [isListModalOpen, setIsListModalOpen] = useState(false);
 
   const board = useSelector((state) => state.board.entities);
 
-
   const [isSideBarOpen, setIsSideBarOpen] = useState(true);
 
   return (
     <div className="board-page">
-    <div className="board-header">
-      <h1>{board.name}</h1>
-      <div className="board-top-line">
-      <hr />
-      </div>
-      
-    </div>
-   
-    <div className={`board-container scrollbar-hide`}>
-     
-      {entities?.lists?.length > 0 ? (
-        <>
-          {entities.lists.map((list, index) => (
-            <TaskList key={index} list={list} colIndex={list._id} workspaceId={workspaceId} boardId={boardId} />
-          ))}
-          <div
-            onClick={() => {
-              setIsListModalOpen(true);
-            }}
-            className="new-column-button"
-          >
-            <img src={VectorIcon} alt="New Column" />
-          </div>
-        </>
-      ) : (
-        <>
-           <div
-            onClick={() => {
-              setIsListModalOpen(true);
-            }}
-            className="new-column-button"
-          >
-            <img src={VectorIcon} alt="New Column" />
-          </div>
-        </>
-      )}
-      {isListModalOpen && (
-        <CreateListModal onClose={()=> setIsListModalOpen(false)} workspaceId={workspaceId} boardId={boardId} />
-      )}
+      <div className="board-header">
+        <div className="board-title">
+          <h1>{board?.name}</h1>
+        </div>
 
-      <button className="add-task-button">Add Task +</button>
-    </div>
+        <div className="board-button-group">
+          <div className="board-filter">
+            <img src={FilterBoardIcon} alt="Filter" />
+          </div>
+
+          <div className="board-members" onClick={toggleModal}>
+            <img src={MembersIcon} alt="Members" />
+          </div>
+
+          <div className="board-share">
+            <img src={ShareIcon} alt="Share" />
+          </div>
+        </div>
+
+        {isModalOpen && (
+          <div className="member-modal" ref={memberModalRef}>
+            <div className="member-modal-title">
+              <h1>Members</h1>
+              <img src={AddMemberIcon} alt="Add Member" />
+            </div>
+            <div className="member-modal-line"></div>
+            <div className="member-modal-content">
+              <div className="member">
+                <img src="" alt="Member 1" />
+                <span>Oğuzhan Demir</span>
+              </div>
+              <div className="member">
+                <img src="" alt="Member 2" />
+                <span>Alperen Yurtseven</span>
+              </div>
+              <div className="member">
+                <img src="" alt="Member 2" />
+                <span>Murat Turoğlu</span>
+              </div>
+              <div className="member">
+                <img src="" alt="Member 2" />
+                <span>Furkan Yıldız</span>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="board-top-line"></div>
+      <hr />
+      <div className={`board-container scrollbar-hide`}>
+        {entities?.lists?.length > 0 ? (
+          <>
+            {entities.lists.map((list, index) => (
+              <TaskList
+                key={index}
+                list={list}
+                colIndex={list._id}
+                workspaceId={workspaceId}
+                boardId={boardId}
+              />
+            ))}
+            <div
+              onClick={() => {
+                setIsListModalOpen(true);
+              }}
+              className="new-column-button"
+            >
+              <img src={VectorIcon} alt="New Column" />
+            </div>
+          </>
+        ) : (
+          <>
+            <div
+              onClick={() => {
+                setIsListModalOpen(true);
+              }}
+              className="new-column-button"
+            >
+              <img src={VectorIcon} alt="New Column" />
+            </div>
+          </>
+        )}
+        {isListModalOpen && (
+          <CreateListModal
+            onClose={() => setIsListModalOpen(false)}
+            workspaceId={workspaceId}
+            boardId={boardId}
+          />
+        )}
+
+        <button className="add-task-button">Add Task +</button>
+      </div>
     </div>
   );
 }
--- src/pages/WorkspacePages/WorkspaceAI.js
+++ src/pages/WorkspacePages/WorkspaceAI.js
@@ -1,196 +1,225 @@
-import React, { useEffect, useState } from 'react';
-import '../../styles/workspaceCss/workspaceAi.css';
+import React, { useEffect, useState } from "react";
+import "../../styles/workspaceCss/workspaceAi.css";
 import WorkspaceTitleİcon from "../../assets/workspaceAi-title-icon.svg";
 import Tickİcon from "../../assets/ai-message-tick-icon.svg";
 import Crossİcon from "../../assets/ai-message-cross-icon.svg";
-import { useParams } from 'react-router-dom';
-import WorkspaceApi from '../../api/BoardApi/workspace.js';
-import ForsicoAiApi from '../../api/ForsicoAiApi/forsicoai.js';
+import { useParams } from "react-router-dom";
+import WorkspaceApi from "../../api/BoardApi/workspace.js";
+import ForsicoAiApi from "../../api/ForsicoAiApi/forsicoai.js";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchWorkspaces } from "../../store/workspaceSlice";
-import { motion, AnimatePresence } from 'framer-motion';
-import { Typewriter } from 'react-simple-typewriter';
+import { motion, AnimatePresence } from "framer-motion";
+import { Typewriter } from "react-simple-typewriter";
 
 const WorkspaceAIPage = () => {
+  const [isEditingDescription, setIsEditingDescription] = useState(false);
+  const [workspaceDescription, setWorkspaceDescription] = useState("");
+  const [isEditingName, setIsEditingName] = useState(false);
+  const { workspaceId } = useParams();
+  const [workspaceIdState, setWorkspaceIdState] = useState(null);
+  const [workspaceName, setWorkspaceName] = useState(useSelector((state)=>state.workspaces.entities.filter((workspace)=> {
+    return workspaceId === workspace._id && workspace.name
+  } )[0].name));
+  const [description, setDescription] = useState(useSelector((state)=>state.workspaces.entities.filter((workspace)=> {
+    return workspaceId === workspace._id && workspace.description
+  } )[0].description));
+  const [aiTasks, setAiTasks] = useState([]);
+  const [visibleTasks, setVisibleTasks] = useState([]);
+  const [taskStates, setTaskStates] = useState({}); // Her kartın durumunu tutmak için
 
-    const [isEditingDescription, setIsEditingDescription] = useState(false);
-    const [workspaceDescription, setWorkspaceDescription] = useState('');
-    const [isEditingName, setIsEditingName] = useState(false);
-    const { workspaceId } = useParams();
-    const [workspaceIdState, setWorkspaceIdState] = useState(null);
-    const [workspaceName, setWorkspaceName] = useState('New Workspace');
-    const [description, setDescription] = useState('New Workspace Desc');
-    const [aiTasks, setAiTasks] = useState([]);
-    const [visibleTasks, setVisibleTasks] = useState([]);
-    const [taskStates, setTaskStates] = useState({}); // Her kartın durumunu tutmak için
+  // Animasyon yapılandırması
+  const staggerAnimation = {
+    visible: {
+      transition: {
+        staggerChildren: 0.5, // Kartların arka arkaya gelme süresi
+      },
+    },
+  };
 
-    // Animasyon yapılandırması
-    const staggerAnimation = {
-        visible: {
-            transition: {
-                staggerChildren: 0.5, // Kartların arka arkaya gelme süresi
-            },
-        },
-    };
+  const cardAnimation = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0 },
+  };
 
-    const cardAnimation = {
-        hidden: { opacity: 0, y: 50 },
-        visible: { opacity: 1, y: 0 },
-    };
+  const dispatch = useDispatch();
+  const token = useSelector((state) => {
+    return state.auth.token.token || "";
+  });
+  const workspaceApi = new WorkspaceApi();
+  const forsicoAiApi = new ForsicoAiApi();
 
-
-    const dispatch = useDispatch();
-    const token = useSelector((state) => {
-        return state.auth.token.token || "";
-    });
-    const workspaceApi = new WorkspaceApi();
-    const forsicoAiApi = new ForsicoAiApi();
-
-    const getAiTasks = async () => {
-        let res = await forsicoAiApi.generateAzureAIContent(workspaceDescription);
-        if (res.success) {
-            setAiTasks(res.data.result.tasks);
-        }
-    };
-
-    useEffect(() => {
-        let index = 0;
-        const interval = setInterval(() => {
-            if (index < aiTasks.length) {
-                setVisibleTasks((prev) => [...prev, aiTasks[index]]);
-                index++;
-            } else {
-                clearInterval(interval);
-            }
-        }, 1000);
-
-        return () => clearInterval(interval);
-    }, [aiTasks]);
-
-    const handleApprove = (taskId) => {
-        setTaskStates((prev) => ({ ...prev, [taskId]: 'approved' }));
-    };
-
-    const handleReject = (taskId) => {
-        setTaskStates((prev) => ({ ...prev, [taskId]: 'rejected' }));
-    };
-
-
-    const workspaceIdSetter = async () => {
-        if (workspaceId === "new") {
-            let response = await workspaceApi.createWorkspace(token, workspaceName, description);
-            console.log(response);
-            if (response.status === true) {
-                setWorkspaceIdState(response.data._id);
-                dispatch(fetchWorkspaces());
-            }
-
-        }
+  const getAiTasks = async () => {
+    let res = await forsicoAiApi.generateAzureAIContent(workspaceDescription);
+    if (res.success) {
+      setAiTasks(res.data.result.tasks);
     }
+  };
 
-    useEffect(() => {
-        workspaceIdSetter();
-    }, [workspaceId])
-    // Workspace adı değişikliği
+  useEffect(() => {
+    let index = 0;
+    const interval = setInterval(() => {
+      if (index < aiTasks.length) {
+        setVisibleTasks((prev) => [...prev, aiTasks[index]]);
+        index++;
+      } else {
+        clearInterval(interval);
+      }
+    }, 1000);
 
+    return () => clearInterval(interval);
+  }, [aiTasks]);
 
-    const handleNameSaveClick = async () => {
-        let response = await workspaceApi.updateWorkspace(token, workspaceIdState, workspaceName, description);
-        console.log(response);
-        if (response.status === true) {
-            dispatch(fetchWorkspaces());
-        }
-        setIsEditingName(false);
-    };
+  const handleApprove = (taskId) => {
+    setTaskStates((prev) => ({ ...prev, [taskId]: "approved" }));
+  };
 
-    const handleDescriptionSaveClick = async () => {
-        let response = await workspaceApi.updateWorkspace(token, workspaceIdState, workspaceName, description);
-        console.log(response);
-        if (response.status === true) {
-            dispatch(fetchWorkspaces());
-        }
-        setIsEditingName(false);
-        setIsEditingDescription(false);
-    };
+  const handleReject = (taskId) => {
+    setTaskStates((prev) => ({ ...prev, [taskId]: "rejected" }));
+  };
 
-    const handleNameChange = (e) => {
-        setWorkspaceName(e.target.value);
-    };
+  const workspaceIdSetter = async () => {
+    if (workspaceId === "new") {
+      let response = await workspaceApi.createWorkspace(
+        token,
+        workspaceName,
+        description
+      );
+      console.log(response);
+      if (response.status === true) {
+        setWorkspaceIdState(response.data._id);
+        dispatch(fetchWorkspaces());
+      }
+    }
+  };
 
-    const handleNameEditClick = () => {
-        setIsEditingName(true);
-    };
+  useEffect(() => {
+    workspaceIdSetter();
+  }, [workspaceId]);
+  // Workspace adı değişikliği
 
-    const handleDescriptionEditClick = () => {
-        setIsEditingDescription(true);
-    };
+  const handleNameSaveClick = async () => {
+    let response = await workspaceApi.updateWorkspace(
+      token,
+      workspaceId,
+      workspaceName,
+      description
+    );
+    console.log(response);
+    if (response.status === true) {
+      dispatch(fetchWorkspaces());
+    }
+    setIsEditingName(false);
+  };
 
-    const handleDescriptionChange = (e) => {
-        setDescription(e.target.value);
-    };
+  const handleDescriptionSaveClick = async () => {
+    let response = await workspaceApi.updateWorkspace(
+      token,
+      workspaceId,
+      workspaceName,
+      description
+    );
+    console.log(response);
+    if (response.status === true) {
+      dispatch(fetchWorkspaces());
+    }
+    setIsEditingName(false);
+    setIsEditingDescription(false);
+  };
 
+  const handleNameChange = (e) => {
+    setWorkspaceName(e.target.value);
+  };
 
+  const handleNameEditClick = () => {
+    setIsEditingName(true);
+  };
 
+  const handleDescriptionEditClick = () => {
+    setIsEditingDescription(true);
+  };
 
-    return (
-        <div className="workspaceAi-container">
-            <div className="workspaceAi-top">
-                <div className='workspaceAi-title-div'>
-                    {isEditingName ? (
-                        <>
-                            <input
-                                className="workspaceAi-title-input"
-                                type="text"
-                                placeholder="Workspace Name"
-                                onChange={handleNameChange}
-                                value={workspaceName}
-                            />
-                            <button className="workspaceAi-save-btn" onClick={handleNameSaveClick}>Save</button>
-                        </>
-                    ) : (
-                        <>
-                            <span className="workspaceAi-title" onClick={handleNameEditClick}>{workspaceName}</span>
-                            <img src={WorkspaceTitleİcon} alt="Workspace Icon" />
-                        </>
-                    )}
-                </div>
+  const handleDescriptionChange = (e) => {
+    setDescription(e.target.value);
+  };
 
-                <div className='workspaceAi-description-div'>
-                    {isEditingDescription ? (
-                        <>
-                            <input
-                                className="workspaceAi-description-input"
-                                type="text"
-                                placeholder="Description"
-                                onChange={handleDescriptionChange}
-                                value={description}
-                            />
-                            <button className="workspaceAi-description-save-btn" onClick={handleDescriptionSaveClick}>Save</button>
-                        </>
-                    ) : (
-                        <>
-                            <span className="workspaceAi-description" onClick={handleDescriptionEditClick}>{description}</span>
-                        </>
-                    )}
-                </div>
+  return (
+    <div className="workspaceAi-container">
+      <div className="workspaceAi-top">
+        <div className="workspaceAi-title-div">
+          {isEditingName ? (
+            <>
+              <input
+                className="workspaceAi-title-input"
+                type="text"
+                placeholder="Workspace Name"
+                onChange={handleNameChange}
+                value={workspaceName}
+              />
+              <button
+                className="workspaceAi-save-btn"
+                onClick={handleNameSaveClick}
+              >
+                Save
+              </button>
+            </>
+          ) : (
+            <>
+              <span className="workspaceAi-title" onClick={handleNameEditClick}>
+                {workspaceName}
+              </span>
+              <img src={WorkspaceTitleİcon} alt="Workspace Icon" />
+            </>
+          )}
+        </div>
 
-                <div className="workspaceAi-input-div">
-                    <textarea
-                        className="workspaceAi-input"
-                        value={workspaceDescription}
-                        onChange={(e) => setWorkspaceDescription(e.target.value)}
-                        placeholder="Describe the job..."
-                    />
-                    <button
-                        className="workspaceAi-generate-btn"
-                        onClick={getAiTasks}
-                        disabled={workspaceDescription.trim() === ''}
-                    >
-                        Generate
-                    </button>
-                </div>
+        <div className="workspaceAi-description-div">
+          {isEditingDescription ? (
+            <>
+              <input
+                className="workspaceAi-description-input"
+                type="text"
+                placeholder="Description"
+                onChange={handleDescriptionChange}
+                value={description}
+              />
+              <button
+                className="workspaceAi-description-save-btn"
+                onClick={handleDescriptionSaveClick}
+              >
+                Save
+              </button>
+            </>
+          ) : (
+            <>
+              <span
+                className="workspaceAi-description"
+                onClick={handleDescriptionEditClick}
+              >
+                {description}
+              </span>
+            </>
+          )}
+        </div>
 
-                <motion.div
+        <div className="textarea-container">
+          <textarea
+            className="workspaceAi-input"
+            value={workspaceDescription}
+            onChange={(e) => setWorkspaceDescription(e.target.value)}
+            placeholder="Describe the job..."
+          />
+          <div className="textarea-icons">
+            <button
+              className="workspaceAi-generate-btn"
+              onClick={getAiTasks}
+              disabled={workspaceDescription.trim() === ""}
+            >
+              Generate
+            </button>
+          </div>
+        </div>
+
+        <motion.div
           className="workspaceAi-message"
           variants={staggerAnimation}
           initial="hidden"
@@ -199,7 +228,9 @@ const WorkspaceAIPage = () => {
           <AnimatePresence>
             {visibleTasks.map((task) => (
               <div key={task.id}>
-                <h1 className='blue-letter workspaceAi-response-title'>{task.name}</h1>
+                <h1 className="blue-letter workspaceAi-response-title">
+                  {task.name}
+                </h1>
 
                 {task.subtasks.map((subtask) => (
                   <motion.div
@@ -207,30 +238,48 @@ const WorkspaceAIPage = () => {
                     variants={cardAnimation}
                     exit={{ opacity: 0, scale: 0 }}
                     transition={{ duration: 0.5 }}
-                    className={`workspaceAi-task ${taskStates[subtask.id] === 'rejected' ? 'rejected-task' : ''}`}
+                    className={`workspaceAi-task ${
+                      taskStates[subtask.id] === "rejected"
+                        ? "rejected-task"
+                        : ""
+                    }`}
                   >
                     <div className="workspaceAi-task-card">
                       <div className="task-header">
-                        <Typewriter words={[subtask.name]} cursor={false} typeSpeed={10} />
+                        <Typewriter
+                          words={[subtask.name]}
+                          cursor={false}
+                          typeSpeed={10}
+                        />
                       </div>
                       <div className="task-desc">
-                        <Typewriter words={[subtask.description]} cursor={false} typeSpeed={10} />
+                        <Typewriter
+                          words={[subtask.description]}
+                          cursor={false}
+                          typeSpeed={10}
+                        />
                       </div>
-                      <div className='task-tags'>
-                        <span className='task-tag'>
-                          {subtask.type.replace(/[^A-Z,a-z]/g, ' ')}
+                      <div className="task-tags">
+                        <span className="task-tag">
+                          {subtask.type.replace(/[^A-Z,a-z]/g, " ")}
                         </span>
-                        <span className='task-tag'>{subtask.assignee}</span>
+                        <span className="task-tag">{subtask.assignee}</span>
                       </div>
                     </div>
                     <div className="task-icons">
-                      {taskStates[subtask.id] !== 'approved' && (
-                        <span className="task-icon" onClick={() => handleReject(subtask.id)}>
+                      {taskStates[subtask.id] !== "approved" && (
+                        <span
+                          className="task-icon"
+                          onClick={() => handleReject(subtask.id)}
+                        >
                           <img src={Crossİcon} alt="cross" />
                         </span>
                       )}
-                      {taskStates[subtask.id] !== 'rejected' && (
-                        <span className="task-icon" onClick={() => handleApprove(subtask.id)}>
+                      {taskStates[subtask.id] !== "rejected" && (
+                        <span
+                          className="task-icon"
+                          onClick={() => handleApprove(subtask.id)}
+                        >
                           <img src={Tickİcon} alt="tick" />
                         </span>
                       )}
@@ -241,9 +290,9 @@ const WorkspaceAIPage = () => {
             ))}
           </AnimatePresence>
         </motion.div>
-            </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default WorkspaceAIPage;
