2366b7a19a58999455e57c71359cb6e88b6b29ad
Mt0666 <118384004+Mt0666@users.noreply.github.com>
2024-10-29 18:56:34 +0300

    introduction updated

--- src/components/LandingPage/Introduction.js
+++ src/components/LandingPage/Introduction.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../../styles/introduction.css'
 import UpperRight from "../../assets/home-page-upper-right.svg"
 import MiddleArea from "../../assets/home-page-middle-area.svg"
+import İntroductionCard from "../../assets/introduction-card.svg"
 
 const Introduction = () => {
     return (
@@ -14,7 +15,19 @@ const Introduction = () => {
                 </div>
                 <div className='introduction-design'><img src={UpperRight} alt="" /></div>
             </div>
-            <div className='introduction-board'><img src={MiddleArea} alt="" /></div>
+            <div className='introduction-board'>
+                <div className='introduction-board-main'>
+                    <div className='board-title-name bg-blue'><span className='board-title-name-text'>General</span></div>
+                    <div className='board-title-name bg-turkuoise'><span className='board-title-name-text'>Doing</span></div>
+                    <div className='board-title-name bg-green'><span className='board-title-name-text'>Done</span></div>
+                </div>
+                <div className='introduction-board-card-main'>
+                    <div className='introduction-board-card board-card-icon'><img className='introduction-card-icon' src={İntroductionCard} alt="card" /></div>
+                    <div className='introduction-board-card'></div>
+                    <div className='introduction-board-card'></div>
+
+                </div>
+            </div>
 
         </div>
     );
